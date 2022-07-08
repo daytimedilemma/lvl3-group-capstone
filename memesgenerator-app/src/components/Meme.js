0d0df758e4e49d1d41096bc5cb21e49acd95bc44
Nicholas Miner <nickaminer94@gmail.com>
2022-07-08 14:57:44 -0600

    Nick made the Add Meme Button function

--- memesgenerator-app/src/components/Meme.js
+++ memesgenerator-app/src/components/Meme.js
@@ -10,7 +10,7 @@ export default function Meme() {
         memeUrl: ""
     });
     const [allMemes, setAllMemes] = React.useState([]);
-
+   
     // Retrieve the meme list from the api
     React.useEffect(() => {
         axios.get("https://api.imgflip.com/get_memes")
@@ -52,6 +52,33 @@ export default function Meme() {
         })
     }
 
+   
+    //Added state for memes list - Nick
+
+    const [memesList, setMemesList] = React.useState([])
+    const memeUnorderedlist = memesList.map((name, index) =>{
+
+        const imgStyle = {
+            width: "200px",
+            height: "200px",
+        }
+
+
+        return (
+            <div key={index}>
+                <img src={name.memeUrl} style={imgStyle}/>
+                <p>{name.topText}</p>
+                <p>{name.bottomText}</p>
+                <button>Delete</button>
+            </div>
+        )
+    })
+    function addMemeList(){
+        setMemesList(prevMemeList => {
+            return [...prevMemeList, meme]
+        })
+    }
+    
     return (
         <section>
             <form>
@@ -71,10 +98,16 @@ export default function Meme() {
                 />
             </form>
             <button onClick={setNewMemeUrl}>Get a random meme</button>
+            <button onClick={addMemeList}>Add Meme</button>
 
+            <div>
             <img src={meme.memeUrl} className="meme-image" />
             <p>{meme.topText}</p>
             <p>{meme.bottomText}</p>
+            </div>
+            <ol>
+               {memeUnorderedlist} 
+            </ol>
         </section>
     );
 }
