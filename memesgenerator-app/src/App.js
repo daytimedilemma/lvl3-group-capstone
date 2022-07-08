834723212bab2c42484812c50f4f18798e9b59e5
Nicholas Miner <nickaminer94@gmail.com>
2022-07-08 11:59:41 -0600

    Intial navBar commit

--- memesgenerator-app/src/App.js
+++ memesgenerator-app/src/App.js
@@ -1,6 +1,9 @@
 import React from "react"
+import Navbar from "./components/Navbar"
 export default function App() {
     return (
-        <h1>Testing</h1>
+        <div>
+            <Navbar />
+        </div>
     )
 }
