bf09b160e72be977b28f5181e6c7c8e0982639a7
michael davis <davis_michael.89@yahoo.com>
2022-07-09 13:04:42 -0700

    update for pull

--- memesgenerator-app/src/components/Meme.js
+++ memesgenerator-app/src/components/Meme.js
@@ -107,8 +107,8 @@ export default function Meme() {
             <img src={meme.memeUrl} className="meme--image" />
             <h2 className="meme--text top">{meme.topText}</h2>
             <h2 className="meme--text bottom">{meme.bottomText}</h2>
-
             </div>
+            
         </main>
     );
 }
